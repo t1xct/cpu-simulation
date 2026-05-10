5e5e194098487a1cb6ace2a8e732ec5817b7a13f
tala <alwailyt@gmail.com>
2026-05-10 21:20:00 +0300

    Create GanttEntry.java

---
+++ GanttEntry.java
@@ -0,0 +1,11 @@
+public class GanttEntry {
+    int processID;
+    int startTime;
+    int endTime;
+
+    public GanttEntry(int p, int sT, int eT) {
+        processID = p;
+        startTime = sT;
+        endTime = eT;
+    }
+}
